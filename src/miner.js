d231953f025dcb8324241e2845e950eb3f7a0a45
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-06-09 01:24:59 +0200

    mine

--- src/miner.js
+++ src/miner.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 import Reversi from './reversi'
 class Miner {
 
@@ -16,18 +17,20 @@ class Miner {
     this.X0Sym = 0
     this.XYSym = 0
     this.XnYSym = 0
+    this.log = 'output.log'
   }
 
-  startMining() {
-    this.mine().then(this.startMining.bind(this))
+  startMining(log) {
+    if (log !== this.log) this.log = log
+    this.mine()
   }
 
   mine() {
-    return new Promise((resolve, reject) => {
       this.date = new Date()
       if (!this.stopMining) {
 
         if (this.totalGamesMined%1000 == 0) console.log('avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000))
+        if (this.totalGamesMined == 1) console.log('avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000))
 
         this.tempGamesMined++
         this.totalGamesMined++
@@ -35,37 +38,36 @@ class Miner {
         if(this.reversi.symmetrical) {
           if (this.reversi.RotSym) {
             this.RotSym++
-            console.log('RotSym')
+            fs.appendFileSync(this.log, 'RotSym');
           }
           if (this.reversi.Y0Sym) {
             this.Y0Sym++
-            console.log('Y0Sym')
+            fs.appendFileSync(this.log, 'Y0Sym');
           }
           if (this.reversi.X0Sym) {
             this.X0Sym++
-            console.log('X0Sym')
+            fs.appendFileSync(this.log, 'X0Sym');
           }
           if (this.reversi.XYSym) {
             this.XYSym++
-            console.log('XYSym')
+            fs.appendFileSync(this.log, 'XYSym');
           }
           if (this.reversi.XnYSym) {
             this.XnYSym++
-            console.log('XnYSym')
+            fs.appendFileSync(this.log, 'XnYSym');
           }
           this.totalCloversFound++
           this.allGamesMined.push(this.tempGamesMined)
-          console.log('time: ' + (this.date.getTime() - this.lastTime))
+          fs.appendFileSync(this.log, 'time: ' + (this.date.getTime() - this.lastTime));
           this.lastTime = this.date.getTime()
-          console.log('current: ' + this.tempGamesMined)
-          console.log('average: ' + (this.totalGamesMined / this.totalCloversFound))
-          console.log('total: ' + this.totalGamesMined)
+          fs.appendFileSync(this.log, 'current: ' + this.tempGamesMined);
+          fs.appendFileSync(this.log, 'average: ' + (this.totalGamesMined / this.totalCloversFound));
+          fs.appendFileSync(this.log, 'total: ' + this.totalGamesMined);
           this.tempGamesMined = 0
-          console.log(this.reversi.movesString)
+          fs.appendFileSync(this.log, this.reversi.movesString);
         }
       }
-      resolve()
-    })
+      setTimeout(this.mine().bind(this))
   }
 }
 export default new Miner()
